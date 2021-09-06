a606a61e73d8e1466c849737d98b9946592d3f6a
ifoxb <ifoxb@DESENVOLVIMENTO>
2021-09-06 17:16:28 -0300

    Add project files.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            //this.BindingContext = new MainViewModel();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            List<DateTime> DD = new List<DateTime>();
+            DD = XALENDAR.SelectedDates.ToList();
+            int count = DD.Count;
+
+            foreach(var item in DD)
+            {
+
+            }
+        }
+    }
+}
---
+++ App1/App1/MainViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ICommand LerCommand { get; set; }
+
+        public CultureInfo Culture => new CultureInfo("ptr-Br");
+
+        public MainViewModel()
+        {
+            DatasSelecionadas = new ObservableCollection<DateTime>();
+
+            LerCommand = new Command(() => ExecuteLerCommand());
+        }
+
+        public DateTime? _dtChosen;
+        public DateTime? DateChosen
+        {
+            get { return _dtChosen; }
+            set
+            {
+                _dtChosen = value;
+                NotifyPropertyChanged(nameof(DateChosen));
+            }
+        }
+        public DateTime? _seldDate;
+        public DateTime? SelectedDate
+        {
+            get { return _seldDate; }
+            set
+            {
+                _seldDate = value;
+                NotifyPropertyChanged(nameof(SelectedDate));
+            }
+        }
+        public void ExecuteLerCommand()
+        {
+            int count = DatasSelecionadas.Count();
+        }
+
+        private ObservableCollection<DateTime> _atdc;
+        public ObservableCollection<DateTime> Attendances
+        {
+            get { return _atdc; }
+            set
+            {
+                _atdc = value;
+                NotifyPropertyChanged(nameof(Attendances));
+            }
+        }
+
+        private ObservableCollection<DateTime> _dtselec;
+        public ObservableCollection<DateTime> DatasSelecionadas
+        {
+            get { return _dtselec; }
+            set
+            {
+                _dtselec = value;
+                NotifyPropertyChanged(nameof(DatasSelecionadas));
+            }
+        }
+
+        public string _min = DateTime.Now.ToString("yyyy-MM-dd");
+        public string DataInicialMinima
+        {
+            get { return _min; }
+            set
+            {
+                _min = value;
+                NotifyPropertyChanged(nameof(DataInicialMinima));
+            }
+        }
+
+        //private DateTime? _date;
+        //public DateTime? Date
+        //{
+        //    get
+        //    {
+        //        return _date;
+        //    }
+        //    set
+        //    {
+        //        _date = value;
+        //        System.Diagnostics.Debug.WriteLine(_date.ToString());
+        //        NotifyPropertyChanged(nameof(Date));
+        //    }
+        //}
+
+        //public ICommand DateChosen
+        //{
+        //    get
+        //    {
+        //        return new Command((obj) => 
+        //        {
+        //            //System.Diagnostics.Debug.WriteLine(obj as DateTime?);
+        //        });
+        //    }
+        //}
+
+        //private ObservableCollection<XamForms.Controls.SpecialDate> attendances;
+        //public ObservableCollection<XamForms.Controls.SpecialDate> Attendances
+        //{
+        //    get { return attendances; }
+        //    set { attendances = value; NotifyPropertyChanged(nameof(Attendances)); }
+        //}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
